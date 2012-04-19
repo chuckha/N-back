efc287c78a47ffea451ec35d29898f8f134a3cbd
chuck <ha.chuck@gmail.com>
2012-04-18 23:21:48 -0400

    Let's play N-back!

---
+++ queue.js
@@ -0,0 +1,15 @@
+// I saw this game as a queue in my head, over engineering FTW!
+var Queue = function () {
+  this.list = [];
+};
+
+Queue.prototype.queue = function (item) {
+  this.list.push(item);
+  return null;
+};
+
+Queue.prototype.dequeue = function () {
+  return this.list.shift();
+};
+
+
